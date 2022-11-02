79c58a3d31f0ad971e26d0fdf5c7f0087b21551a
Nathan Dias <nathanfaria9@gmail.com>
2022-11-02 18:16:26 -0300

    Primeiros ajustes home

---
+++ ecommerce/src/views/Home.jsx
@@ -0,0 +1,36 @@
+import '../styles/Home.css';
+
+const Home = (props) => {
+    return(
+        <div className="home">
+            <div className="banner-container">
+                <div className='banner-description'>
+                    <h1>THE BEST TECH PRODUCTS HERE</h1>
+                    <p>Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
+                </div>
+            </div>
+            <div className='home-main'>
+                <div className='home-main-first-box'>
+                    <div className='home-main-first-box-content'>
+                        <h2>Quality</h2>
+                        <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
+                    </div>
+                    <div className='home-main-first-box-content'>
+                        <h2>Quality</h2>
+                        <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
+                    </div>
+                    <div className='home-main-first-box-content'>
+                        <h2>Quality</h2>
+                        <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
+                    </div>
+                    <div className='home-main-first-box-content'>
+                        <h2>Quality</h2>
+                        <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
