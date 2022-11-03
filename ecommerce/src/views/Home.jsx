ea55803854fd1c8dc09e98a69eb9970902728346
Nathan Dias <nathanfaria9@gmail.com>
2022-11-03 16:35:15 -0300

    Home-main-second

--- ecommerce/src/views/Home.jsx
+++ ecommerce/src/views/Home.jsx
@@ -6,25 +6,35 @@ const Home = (props) => {
             <div className="banner-container">
                 <div className='banner-description'>
                     <h1>THE BEST TECH PRODUCTS HERE</h1>
-                    <p>Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
+                    <p>Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
                 </div>
             </div>
             <div className='home-main'>
                 <div className='home-main-first-box'>
                     <div className='home-main-first-box-content'>
-                        <h2>Quality</h2>
+                        <h2>Test</h2>
                         <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
                     </div>
                     <div className='home-main-first-box-content'>
-                        <h2>Quality</h2>
+                        <h2>Test</h2>
                         <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
                     </div>
                     <div className='home-main-first-box-content'>
-                        <h2>Quality</h2>
+                        <h2>Test</h2>
                         <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
                     </div>
                     <div className='home-main-first-box-content'>
-                        <h2>Quality</h2>
+                        <h2>Test</h2>
+                        <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
+                    </div>
+                </div>
+                <div className='home-main-second-box'>
+                    <div className='home-main-second-box-content'>
+                        <h2>Test</h2>
+                        <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
+                    </div>
+                    <div className='home-main-second-box-content'>
+                        <h2>Test</h2>
                         <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
                     </div>
                 </div>
